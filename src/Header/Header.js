49596fac8a9c9d4dc13789ea8d0d5731cf88c02a
Rimplstilckin <milos.protic@gmail.com>
2021-05-02 00:21:39 +0200

    Header coding finished

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { Fragment,  useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
 import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../img/logo.svg';
 
@@ -18,41 +21,58 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'left'
   },
   img: {
-    maxWidth: 150
-  },
-  // toolbar: {
-  //   width: "100%"
-  // },
-  content: {
-    [theme.breakpoints.only('xs')]:{
-        display: "none!important",
-      }
-  },
-  menu: {
-    display: "none!important",
-    color: "red",
-      [theme.breakpoints.only('xs')]:{
-          display: "inline!important",
-          padding: 10,
-        }
+    maxWidth: 150,
+    minWidth: 120
   }
 }));
 
-const menu = [{name:"About", key: "oNama"},
-              {name: "Gallery", key:"galerija"},
-              {name: "References", key: "reference"},
-              {name:"Locations", key:"lokacije"}]
 
- export default function Header(){
+const getWidth = () => window.innerWidth
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+
+  function useCurrentWidth() {
+  // save current window width in the state object
+  let [width, setWidth] = useState(getWidth());
+
+  // in this case useEffect will execute only once because
+  // it does not have any dependencies.
+  useEffect(() => {
+    const resizeListener = () => {
+      // change width from the state object
+      setWidth(getWidth())
+    };
+    // set resize listener
+    window.addEventListener('resize', resizeListener);
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener);
+    }
+  }, [])
+
+  return width;
+}
+
+const menu = [{name:"O Direktoru", key: "oNama"},
+              {name: "Galerija", key:"galerija"},
+              {name: "Reference", key: "reference"},
+              {name:"Lokacija", key:"lokacije"}]
+
+export default function Header(){
 
   const [currentState, newState] = useState(false);
-  const menuDrawer = (event) => {
-    console.log(currentState);
+
+  const toggleDrawer = (event) => {
     newState(!currentState);
   }
 
+  let width = useCurrentWidth();
+
   const classes = useStyles();
   const preventDefault = (event) => event.preventDefault();
+  
   return(
     <AppBar
       className={classes.app}
@@ -65,40 +85,66 @@ const menu = [{name:"About", key: "oNama"},
               <img className={classes.img} src={logo} alt="Logo" />
             </Link>
           </Grid>
-          <Grid item
-                className={classes.content}
-                container
-                sm={8}
-                spacing={2}>
-              {menu.map(item=>{
-                return(<Grid item
-                             sm={3}
-                             key={item.key}>
-                          <Link color="inherit"
-                                className={classes.item + " " + classes.xs}
-                                onClick={preventDefault}
-                                href="#">
-                                {item.name}
-                          </Link>
-                        </Grid>)
-               })}
-            </Grid>
-            <Grid item xs={2} className={classes.content}>
-            <Button
-                  component={Link}
-                  variant="contained"
-                  color="blue"
-                  href="#"
-                  className={classes.button}
-                  size="large">
-            Contact
-            </Button>
-            </Grid>
-            <Grid item className={classes.menu} xs={2}>
-              <IconButton className={classes.iconButton} onClick={menuDrawer}>
-                <MenuIcon />
-              </IconButton>
-            </Grid>
+          {
+          width>700 ?
+          <Fragment>
+            <Grid item
+                  className={classes.content}
+                  container
+                  sm={8}
+                  spacing={2}>
+                {menu.map(item=>{
+                  return(<Grid item
+                              sm={3}
+                              key={item.key}>
+                            <Link color="inherit"
+                                  className={classes.item + " " + classes.xs}
+                                  onClick={preventDefault}
+                                  href="#">
+                                  {item.name}
+                            </Link>
+                          </Grid>)
+                })}
+              </Grid>
+              <Grid item xs={2} className={classes.content}>
+              <Button
+                    component={Link}
+                    variant="contained"
+                    color="blue"
+                    href="#"
+                    className={classes.button}
+                    size="large">
+              Contact
+              </Button>
+              </Grid>
+            </Fragment> :
+            <Fragment>
+              <Grid item className={classes.menu} xs={2}>
+                <IconButton className={classes.iconButton} onClick={toggleDrawer}>
+                  <MenuIcon />
+                </IconButton>
+              </Grid>
+              <Drawer
+                anchor="left"
+                open={currentState}
+                onClose={toggleDrawer}>
+                  <div onClick={toggleDrawer}>
+                    <List>
+                      {menu.map(item=>{
+                        return(
+                          <ListItem button key={item.key}>
+                            <Link>
+                              {item.name}
+                            </Link>
+                          </ListItem>
+                        )
+                      }
+                      )}
+                    </List>
+                  </div>
+              </Drawer>
+            </Fragment> 
+            }
         </Grid>
       </Toolbar>
     </AppBar>
