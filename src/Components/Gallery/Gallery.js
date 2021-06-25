4b696b728bf2f43a6cd504c6ef2fa925e5c28fb6
Milos Protic <milos.protic@gmail.com>
2021-06-25 20:13:00 +0200

    Something

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
-const Gallery = () => {
+
+const useStyles = makeStyles((theme) => ({
+  gallery: {
+    margin:'auto'
+  },
+  image: {
+    width: '100%'
+  }
+}));
+
+const gallery = [
+  {name: 'Van', path: './img/gallery/kombi.jpeg'},
+  {name: 'logo', path: './img/gallery/logo.svg'},
+  {name: 'test', path: './img/gallery/test.jpeg'},
+  {name: 'test 2', path: './img/gallery/test.jpeg'},
+]
+
+export default function Gallery(){
+  const classes = useStyles();
   return(
     <div>
-      <h4>Gallery</h4>
+      <h3>Gallery</h3>
+      <Grid
+        className={classes.gallery}
+        container
+        xs={12}
+        spacing={2}
+        alignItems="center"
+        >
+        {gallery.map(item => {
+          return(<Grid
+                  item
+                  xs={6}
+                  sm={3}
+                  key={item.name}
+                  spacing={2}
+                  >
+                    <img
+                    className={classes.image}
+                    alt={item.name}
+                    src={item.path}
+                    />
+                  </Grid>
+                )
+              })}
+      </Grid>
     </div>
   )
 }
-
-export default Gallery;
--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { width } from '../../globals';
 
 const useStyles = makeStyles((theme) => ({
-  map: {
-    width: width,
-    margin: 'auto',
-    padding: '30px'
-  },
-  image: {
-    width: '80%',
-  }
+
 }));
+
+const partners = [
+  {name: 'Van', path: './img/gallery/kombi.jpeg'},
+  {name: 'logo', path: './img/gallery/logo.svg'},
+  {name: 'test', path: './img/gallery/test.jpeg'},
+  {name: 'test 2', path: './img/gallery/test.jpeg'},
+]
 
 export default function Partners(){
   const classes = useStyles();
   return(
-    <h4>Partners</h4>
+    <div className={classes.partners}>
+      <h4>Our content customers</h4>
+      <Grid
+        className={classes.container}
+        xs={12}
+        spacing={2}
+        alignItems="center"
+        >
+        {partners.map(item =>{
+          return(
+            <Grid
+            index={item.name}
+            >
+              <img
+                src={item.path}
+                alt={item.name}
+              />
+            </Grid
+          )
+        })}
+      </Grid>
+    </div>
   )
 }
